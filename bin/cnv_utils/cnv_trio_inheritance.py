9bec3d389807f5a18650e970d8d2171dd2cfe376
Bénitière Florian <34925751+FloBeni@users.noreply.github.com>
2025-12-22 12:38:46 -0500

    correction trio
    there was a bug when multiple overlap, also improving the speed

--- bin/cnv_utils/cnv_trio_inheritance.py
+++ bin/cnv_utils/cnv_trio_inheritance.py
@@ -59,14 +59,13 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-# +
 FILE_CNV = args.cnv
 PEDIGREE_FILE = args.pedigree
 OUTPUT = args.output
 TYPE_COL = args.type_col
 OVERLAPS = [float(x) for x in args.overlap.split(",")]
 
-#Detect input type naively
+# Detect file type (TSV vs Parquet)
 tsv_mode = True
 suffix = Path(FILE_CNV).suffixes
 if '.parquet' in suffix:
@@ -74,20 +73,24 @@ if '.parquet' in suffix:
 elif ".tsv" not in suffix:
     print("Invalid input type or malformed extension")
     sys.exit(1)
-# -
 
-# ------------------- Filter CNV Calls -------------------
 
-# +
-family_info = pl.scan_csv(PEDIGREE_FILE, separator="\t",infer_schema_length=100000)
+# ------------------- Load Pedigree -------------------
+family_info = pl.scan_csv(PEDIGREE_FILE, separator="\t",infer_schema_length=1000000)
+family_info = family_info.with_columns([
+    pl.col("SampleID").cast(pl.Utf8),
+    pl.col("FatherID").cast(pl.Utf8),
+    pl.col("MotherID").cast(pl.Utf8)
+])
+family_info = family_info.select(["SampleID", "FatherID", "MotherID"])
 
-#load CNV lazily
+# ------------------- Load CNVs -------------------
 if TYPE_COL:
     if tsv_mode:
         df_cnv = pl.scan_csv(FILE_CNV, 
                              separator = "\t", 
                              schema_overrides={f"{TYPE_COL}": pl.Categorical, 
-                                           "Chr": pl.Categorical},infer_schema_length=100000)
+                                           "Chr": pl.Categorical},infer_schema_length=1000000)
         print(f"Loading {FILE_CNV} in tsv mode and using {TYPE_COL} as type column")
     #casting to categorical for performance 
     else:
@@ -100,31 +103,31 @@ else:
         print(f"Loading {FILE_CNV} in tsv mode without type column")
         df_cnv = pl.scan_csv(FILE_CNV, 
                              separator = "\t", 
-                             schema_overrides={"Chr": pl.Categorical},infer_schema_length=100000)
+                             schema_overrides={"Chr": pl.Categorical},infer_schema_length=1000000)
     else:
         print(f"Loading {FILE_CNV} in parquet mode and without type column")
         df_cnv = pl.scan_parquet(FILE_CNV)
         df_cnv = df_cnv.with_columns(pl.col("Chr").cast(pl.Categorical))
         
+df_cnv = df_cnv.with_columns(pl.col("SampleID").cast(pl.Utf8))
 
 print("Filtering for trios that are completely found in the cnv table...")
 
-#keep only trios that have CNVs for every member 
-sample_ids = df_cnv.select(pl.col("SampleID")).unique().collect().to_series()                         
+# keep only trios that have CNVs for every member 
+sample_ids = df_cnv.select(pl.col("SampleID")).unique().collect().to_series()
 family_trios =  family_info.filter(
     pl.col("SampleID").is_in(sample_ids.implode()) & #adding implodes as per https://github.com/pola-rs/polars/pull/22178
     pl.col("FatherID").is_in(sample_ids.implode()) &
     pl.col("MotherID").is_in(sample_ids.implode())
 )
 
-
-
-# +
 print("Building bed files...")
 
-#child cnvs subset from those available from pedigree file 
+# ------------------- Subset Child and Parent CNVs -------------------
+# Child CNVs (present in pedigree)
 df_child = df_cnv.join(family_trios, on = "SampleID", how = "inner")
-#father cnvs 
+
+# Parent CNVs
 df_father = df_cnv.join(family_trios, left_on = "SampleID", 
                                      right_on = "FatherID", 
                                      suffix = ".child", 
@@ -136,23 +139,28 @@ df_mother = df_cnv.join(family_trios, left_on = "SampleID",
                                      how = "inner")
 
 
-#reorder to put child ID to front and drop parent IDs
+# Reorder columns and drop redundant IDs
 standard_col = ["SampleID.child","Chr","Start","End"] 
 f_order = standard_col + [col for col in df_father.collect_schema().names() if col not in standard_col]
 m_order = standard_col + [col for col in df_mother.collect_schema().names() if col not in standard_col]
 df_parents = pl.concat([df_father.select(f_order).drop("MotherID","SampleID"), 
                         df_mother.select(m_order).drop("FatherID", "SampleID")])
 
-#build ID column for bedtools with concatentated groups (ie: Chr and type)
+# ------------------- Build BED IDs for bedtools -------------------
 if TYPE_COL is not None:
-
+    # Parents
     parents_out = df_parents.with_columns(pl.concat_str([pl.col("SampleID.child"),
                                                          pl.lit("_"),
                                                          pl.col("Chr"), 
                                                          pl.lit("_"), 
                                                          pl.col(f"{TYPE_COL}")])
                                           .alias("SampleID")).select("SampleID","Start","End")
-
+                    
+    # Children                      
+    df_child = df_child.with_columns(
+        (pl.col("SampleID") + "_" + pl.col("Chr").cast(pl.Utf8) + "_" + pl.col(TYPE_COL) + "_" + pl.col("Start").cast(pl.Utf8) + "_" + pl.col("End").cast(pl.Utf8) ).alias("ID")
+    )
+    
     child_out = df_child.with_columns(pl.concat_str([pl.col("SampleID"),
                                                      pl.lit("_"),
                                                      pl.col("Chr"), 
@@ -169,6 +177,10 @@ else:
                                                         ])
                                           .alias("SampleID")).select("SampleID","Start","End")
 
+    df_child = df_child.with_columns(
+        (pl.col("SampleID") + "_" + pl.col("Chr").cast(pl.Utf8) + "_" + pl.col("Start").cast(pl.Utf8) + "_" + pl.col("End").cast(pl.Utf8) ).alias("ID")
+    )
+
     child_out = df_child.with_columns(pl.concat_str([pl.col("SampleID"),
                                                      pl.lit("_"),
                                                      pl.col("Chr"), 
@@ -176,7 +188,8 @@ else:
                                                      ])
                                       .alias("SampleID")).select("SampleID","Start","End")
 
-#print out as bed files
+
+# ------------------- Write BED files -------------------
 child_bed = f"child_cnv.bed"
 parent_bed = f"parents_cnv.bed"
 
@@ -184,128 +197,51 @@ child_out.sink_csv(child_bed, separator = "\t", include_header = False)
 parents_out.sink_csv(parent_bed, separator = "\t", include_header = False)
 
 
-# -
-
 # ------------------- Run bedtools intersect -------------------
-
 for ovlap in OVERLAPS:
-    bash_cmd = f"bedtools intersect -a {child_bed} -b {parent_bed} -f {ovlap} -r -wa -u > intersect_ovlap{ovlap}.bed;\
-bedtools intersect -a {child_bed} -b {parent_bed} -f {ovlap} -r -v > non_intersect_ovlap{ovlap}.bed"
+    bash_cmd = f"bedtools intersect -a {child_bed} -b {parent_bed} -f {ovlap} -r -wa -u> intersect_ovlap{ovlap}.bed;"
     print("Running command:\n", bash_cmd)
     subprocess.run(bash_cmd, shell=True, check=True)
 
-# +
 print("Formatting intersections and combining results...")
 
-#load non-intersects and intersects and add the overlap columns, all false for non-overlap and vice-versa
-ovl_count = len(OVERLAPS)
-count = 0
-#for multiple overlap attempts we need to build a single table using multiple joins
-while count < ovl_count:
-    #for the first file no join needed 
-    if(count == 0):
-        non_intersects_file = pl.scan_csv(f"non_intersect_ovlap{OVERLAPS[count]}.bed", 
-                                          separator = "\t", has_header = False,infer_schema_length=100000)
-        #all false from non-interesects
-        non_intersects = non_intersects_file.with_columns(pl.lit(False)
-                                                          .alias(f"Observed_in_Parent_{OVERLAPS[count]}"))
-        #all true from intersects
-        intersects_file = pl.scan_csv(f"intersect_ovlap{OVERLAPS[count]}.bed", separator = "\t", has_header = False,infer_schema_length=100000)
-        intersects = intersects_file.with_columns(pl.lit(True).alias(f"Observed_in_Parent_{OVERLAPS[count]}"))
+# ------------------- Annotate Child CNVs with Overlaps -------------------
+for ovlap in OVERLAPS:
+        intersects = pl.scan_csv(f"intersect_ovlap{ovlap}.bed", separator = "\t", has_header = False,infer_schema_length=1000000)
         
-        count += 1
-    else:
-        #non-intersects
-        non_intersects_file = pl.scan_csv(f"non_intersect_ovlap{OVERLAPS[count]}.bed", 
-                                          separator = "\t", has_header = False,infer_schema_length=100000)
-        
-        non_intersects_file = non_intersects_file.with_columns(pl.lit(False)
-                                                          .alias(f"Observed_in_Parent_{OVERLAPS[count]}"))
-        #join on all matching columns to add new intersect column
-        non_intersects = non_intersects.join(non_intersects_file, on = ["column_1","column_2","column_3"])
-
-        
-        #intersects file
-        intersects_file = pl.scan_csv(f"intersect_ovlap{OVERLAPS[count]}.bed", 
-                                      separator = "\t", has_header = False,infer_schema_length=100000)
-        
-        intersects_file = intersects_file.with_columns(pl.lit(True).alias(f"Observed_in_Parent_{OVERLAPS[count]}"))
-        intersects = intersects.join(intersects_file, on = ["column_1","column_2","column_3"])
-        count += 1
-        
+        # Build ID column for matching
+        intersects = intersects.with_columns(
+         (pl.col("column_1") + "_" + pl.col("column_2").cast(pl.Utf8) + "_" + pl.col("column_3").cast(pl.Utf8) ).alias("ID")
+          )
+          
+    # Join with child CNVs and mark True/False
+        df_child = df_child.join(
+              intersects.select("ID").with_columns(
+                  pl.lit(True).alias(f"Observed_in_Parent_{ovlap}")
+              ),
+              on="ID",
+              how="left"
+          ).with_columns(
+              pl.col(f"Observed_in_Parent_{ovlap}").fill_null(False)
+          )
 
 
-#concat the two files
-lookup = pl.concat([intersects, non_intersects])
-
-
-lookup_id_fields  = ["SampleID", "Chr"]
-new_lookup_cols = ["SampleID", "Chr", "Start", "End"]
-
-if TYPE_COL:
-    lookup_id_fields += [f"{TYPE_COL}"]
-    new_lookup_cols += [f"{TYPE_COL}"]
-
-#print(new_lookup_cols)
-#reformat and deconstruct the id field
-lookup = (lookup.with_columns(
-            pl.col("column_1")
-            .str.split_exact("_", 2)
-            .struct.rename_fields(lookup_id_fields)
-            .alias("ID_fields")
-    ).unnest("ID_fields").drop("column_1")
-)
-
-
-lookup_dict = {"column_2" : "Start", "column_3" : "End"}
-lookup = lookup.rename(lookup_dict)
-
-
-
-if TYPE_COL:
-    #cast back to make the next join work
-    lookup = lookup.with_columns(pl.col("Chr").cast(pl.Categorical),
-                                 pl.col(f"{TYPE_COL}").cast(pl.Categorical))
-else:
-    lookup = lookup.with_columns(pl.col("Chr").cast(pl.Categorical))
-
-#Make sure the SampleID in the Child CNVs df is string to match with original child cnv dataframe
-df_child  = df_child.with_columns(pl.col("SampleID").cast(pl.Utf8))
-
-
-#perform join on child CNV columns 
-result = df_child.join(lookup, on = new_lookup_cols,
-                               how = "inner",
-                               coalesce=True)
-
-
-#reorder for final output 
-standard_col = ["SampleID","Chr","Start","End"] 
-f_order = standard_col + [col for col in result.collect_schema().names() if col not in standard_col]
-
-#sink
-# result.select(f_order).drop("FatherID", "MotherID").sink_csv(f"{OUTPUT}", separator = "\t")
-# Collect pedigree file column names
-pedigree_cols = family_info.collect_schema().names()
-
-# Keep only SampleID from pedigree
-cols_to_drop = [col for col in pedigree_cols if col != "SampleID"]
-print(cols_to_drop)
-# Drop them if they exist in the final DataFrame
-lookup = result.select(f_order).drop(cols_to_drop, strict=False).sink_csv(f"{OUTPUT}", separator = "\t")
-
+# Drop temporary ID column
+df_child = df_child.drop("ID")
 
 print("Pipeline completed successfully.")
 
 
+# ------------------- Write Final Annotated CNVs -------------------
+df_child.drop("FatherID", "MotherID").sink_csv(f"{OUTPUT}", separator = "\t")
 print(f"💾 Results written to {OUTPUT}")
 
 
-# --------------------- Cleanup Temporary Files ---------------------
+# ------------------- Cleanup Temporary Files -------------------
 files_to_remove = [
     parent_bed,
     child_bed
-] + [f"intersect_ovlap{ov}.bed" for ov in OVERLAPS] + [f"non_intersect_ovlap{ov}.bed" for ov in OVERLAPS]
+] + [f"intersect_ovlap{ov}.bed" for ov in OVERLAPS] 
 
 for f in files_to_remove:
     try:
